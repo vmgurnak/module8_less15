f9268e6892bfab999b7293c0fe21825b38cf36ff
Valeriy.Gurnak <vmgurnak@gmail.com>
2023-08-23 10:37:54 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
+// Переменные обновлять только через const или let, во время минификации сборки после отправки на git все необъявленные переменные удаляются
+
 let count = 0;
 for (let i = 0; i < 10; i += 1) {
   count += 1;
   console.log(count);
 }
+
+const test = 'Hello world';
+console.log(test);
